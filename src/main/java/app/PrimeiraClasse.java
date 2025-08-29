c2eb13cb38096f1cb705bcbef1a26ab74d88217a
NelsonGilberto1 <168493132+NelsonGilberto1@users.noreply.github.com>
2025-08-29 01:46:47 +0000

    Aula 28.08.2025

--- src/main/java/app/PrimeiraClasse.java
+++ src/main/java/app/PrimeiraClasse.java
@@ -10,5 +10,26 @@ package app;
             System.out.println(a1.getIdade());
             
             System.out.println("Olá mundo!!!!");
+
+            Tarefa t1 = new Tarefa();
+            t1.setDescricao("Estudar Estruturas de Dados");
+            t1.setConcluido(true);
+
+            Tarefa t2 = t1;
+            t2.setConcluido(false);
+
+            System.out.println(t1.getDescricao());
+            System.out.println(t1.isConcluido());
+
+            Atividade[] objetos = new Atividade[3];
+
+            Calendar c = Calendar.getInstance();
+            c.set(2025, 9, 28);
+
+            objetos[0] = new Compromisso("Novo Compromisso", c, c);
+            objetos[1] = new Tarefa("Nova Tarefa", true);
+
+            System.out.println(objetos[0].getDetalhes());
+            System.out.println(objetos[1].getDetalhes());
         }
     }
